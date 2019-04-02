b975717b71e3e660ae844831793229d2a8bed5e9
runescapejon <jonsimpletv@gmail.com>
2019-04-02 09:00:09 -0400

    Update Configuration.java

--- src/main/java/me/runescapejon/main/Configuration.java
+++ src/main/java/me/runescapejon/main/Configuration.java
@@ -5,6 +5,6 @@ import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
 @ConfigSerializable
 public class Configuration {
-	@Setting(value = "Message")
+	@Setting(value = "Message", comment = "Placeholder for playername is %player%"))
 	public static String Message = "&6&lYour whole inventory was saved from your recent Death.";
 }
